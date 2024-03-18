9f4beee53e7f1c6c67e4e23ed999b5a7c23b94cb
Mikkel Arentoft <mikkel@mikadocs.com>
2024-03-18 17:10:31 +0100

    Initial cut on CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// List all tapes
+    List,
+    /// Create a new tape
+    Create {
+        /// The tape length
+        length: u8,
+    },
+    /// Read a tape
+    Read {
+        /// The tape ID
+        id: String,
+    },
+
+    /// Update a tape
+    Update {
+        /// The tape ID
+        id: String,
+        /// The tape length
+        length: u8,
+    },
+    /// Delete a tape
+    Delete {
+        /// The tape ID
+        id: String,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let cli = Cli::parse();
+
+    Ok(())
 }
